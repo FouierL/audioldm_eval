e9a890f61cd959ed9ff6a2a7d3475a3544b83d3e
Chenxu Wu <727004430@qq.com>
2025-10-08 12:40:45 +0800

    Refactor evaluation setup in test.py to improve multi-threading support and update paths for generation and target audio. Switch to using EvaluationHelperParallel with a specified backbone for enhanced performance.

--- test.py
+++ test.py
@@ -1,28 +1,23 @@
 import torch
-from audioldm_eval import EvaluationHelper, EvaluationHelperParallel
-import torch.multiprocessing as mp
+from audioldm_eval import EvaluationHelperParallel
+import torch.multiprocessing as mps
+import os
 
-device = torch.device(f"cuda:{0}")
+cpu_num = 1
+os.environ['OMP_NUM_THREADS'] = str(cpu_num)
+os.environ['OPENBLAS_NUM_THREADS'] = str(cpu_num)
+os.environ['MKL_NUM_THREADS'] = str(cpu_num)
+os.environ['VECLIB_MAXIMUM_THREADS'] = str(cpu_num)
+os.environ['NUMEXPR_NUM_THREADS'] = str(cpu_num)
+torch.set_num_threads(cpu_num)
+torch.multiprocessing.set_sharing_strategy('file_system')
 
-generation_result_path = "example/paired"
-# generation_result_path = "example/unpaired"
-target_audio_path = "example/reference"
-
-## Single GPU
-
-evaluator = EvaluationHelper(16000, device)
-
-# Perform evaluation, result will be print out and saved as json
-metrics = evaluator.main(
-    generation_result_path,
-    target_audio_path,
-)
-
-## Multiple GPUs
+generation_result_path = "/data2/chenxuwu/DiT-MoE-Video2Audio-v2/outputs/mel/ourdit_concat/CFG4.5_euler_26gen_wav_16k_80"
+target_audio_path = "/data2/chenxuwu/DiT-MoE-Video2Audio-v2/outputs/audio_test_aligned"
 
 if __name__ == '__main__':    
-    evaluator = EvaluationHelperParallel(16000, 2)
+    evaluator = EvaluationHelperParallel(16000, 2, backbone="cnn14") # 使用1个GPU避免内存竞争
     metrics = evaluator.main(
         generation_result_path,
-        target_audio_path,
+        target_audio_path
     )
