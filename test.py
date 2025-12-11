bf163c01fcde9513553c526b5ee5397a88ede347
Chenxu Wu <727004430@qq.com>
2025-12-11 21:55:02 +0800

    参数更新

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ os.environ['NUMEXPR_NUM_THREADS'] = str(cpu_num)
 torch.set_num_threads(cpu_num)
 torch.multiprocessing.set_sharing_strategy('file_system')
 
-generation_result_path = "/data2/chenxuwu/DiT-MoE-Video2Audio-v2/outputs/mel/eq_ft_moe/CFG4.5_euler_26_bw0.00gen_wav_16k_80"
+generation_result_path = "/data2/chenxuwu/DiT-MoE-Video2Audio-v2/outputs/mel/latent_tokenizer_22/CFG4.5_euler_26gen_wav_16k_80"
 target_audio_path = "/data2/chenxuwu/DiT-MoE-Video2Audio-v2/outputs/audio_test_aligned"
 
 if __name__ == '__main__':    
